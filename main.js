196f7997c7030b3c518bdf924d7df01891775e43
michael bartosz <michaelbartosz@michaels-iMac.local>
2020-06-04 20:00:48 -0400

    working final

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
-
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
@@ -15,24 +12,31 @@ const rowElements = document.querySelectorAll(".row")
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+function getCellElements(currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
-
 // YOUR CODE GOES HERE
 
+replaceAllButton.addEventListener("click", function () {
+    let findUserInput = findInput.value
+    let replaceUserInput = replaceInput.value
+    for (let counter = 0; counter < rowElements.length; counter = counter + 1) {
+        let currentRowElement = rowElements[counter]
+        let currentRowElementCells = getCellElements(currentRowElement)
+        
+        for (let counter2 = 0; counter2 < currentRowElementCells.length; counter2 = counter2 + 1) {
+            let currentRowCellElement = currentRowElementCells[counter2]
+            console.log(currentRowCellElement)
 
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
+            if (currentRowCellElement.innerHTML.includes(findUserInput)) {
+                let NewString = currentRowCellElement.innerHTML.replace(findUserInput, replaceUserInput)
+                currentRowCellElement.innerHTML = NewString
+
+            }
+        }
+    }
+})
+
 // You can, of course, remove any comments in this starter project once
 // you have read them, if you prefer.
